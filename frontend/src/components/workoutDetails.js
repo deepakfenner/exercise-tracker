b9c11638e20ae0375a3ebe7daff9e089bbd4e7fc
deepakfenner <deep21110.cd@rmkec.ac.in>
2024-08-14 21:07:59 +0530

    new

--- frontend/src/components/workoutDetails.js
+++ frontend/src/components/workoutDetails.js
@@ -1,5 +1,5 @@
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
-
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 const WorkoutDetails=(({workout})=>{
     const{dispatch} =useWorkoutsContext()
 
@@ -18,8 +18,8 @@ const WorkoutDetails=(({workout})=>{
             <h4>{workout.title}</h4>
             <p><strong>Load (Kg): </strong>{workout.load}</p>
             <p><strong>Reps: </strong>{workout.reps}</p>
-            <p>{workout.createdAt}</p>
-            <span onClick={handleClick}>Delete</span>
+            <p>{formatDistanceToNow(new Date(workout.createdAt),{addSuffix:true})}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 })
